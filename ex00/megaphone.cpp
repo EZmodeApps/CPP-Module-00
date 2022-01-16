c1f0c3da5a8705251a224ded015fb6ace7231b24
EZmodeApps <75046274+EZmodeApps@users.noreply.github.com>
2022-01-16 18:28:16 +0300

    ex00 done

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -2,10 +2,13 @@
 
 int main(int argc, char **argv)
 {
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	for (int i = 1; i < argc; i++)
 	{
-		std::cout << argv[i];
-		std::cout << "\n";
+		for (int j = 0; j < strlen(argv[i]); j++)
+			std::cout << (char)toupper(argv[i][j]);
 	}
+	std::cout << std::endl;
 	return (0);
 }
