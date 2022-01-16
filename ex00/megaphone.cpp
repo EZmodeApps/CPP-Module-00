773ab47fd40d59d2ff26c11f0d287ca0e0c8ab21
EZmodeApps <75046274+EZmodeApps@users.noreply.github.com>
2022-01-16 21:52:07 +0300

    added makefile, character output fix

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,8 +6,11 @@ int main(int argc, char **argv)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	for (int i = 1; i < argc; i++)
 	{
-		for (int j = 0; j < strlen(argv[i]); j++)
-			std::cout << (char)toupper(argv[i][j]);
+		for (int j = 0; j < (int)strlen(argv[i]); j++)
+			if (argv[i][j] >= 'a' && argv[i][j] <= 'z')
+				std::cout << (char)toupper(argv[i][j]);
+			else
+				std::cout << argv[i][j];
 	}
 	std::cout << std::endl;
 	return (0);
