f405c5ee88c9c0bc69351d1b9adead64efdf29cd
EZmodeApps <75046274+EZmodeApps@users.noreply.github.com>
2022-03-21 21:33:51 +0300

    show contact upd

--- ex01/contact.cpp
+++ ex01/contact.cpp
@@ -15,6 +15,7 @@ void	Contact::showContact(void)
 	std::cout << "|" << std::setw(10) << info[0];
 	std::cout << "|" << std::setw(10) << info[1];
 	std::cout << "|" << std::setw(10) << info[2];
+	std::cout << std::endl;
 }
 
 bool Contact::setContact(int index) {
@@ -24,8 +25,8 @@ bool Contact::setContact(int index) {
 //	std::getline(std::cin, info[2]);
 	for (int i = 0; i <= 2; i++)
 		std::getline(std::cin, info[i]);
-	for (int i = first_name; i <= 2; i++)
-		std::cout << info[i] << std::endl;
+//	for (int i = first_name; i <= 2; i++)
+//		std::cout << info[i] << std::endl;
 	//if длина строки == 0 то false
 	return (true);
 }
