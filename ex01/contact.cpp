f780f9612f1c8820f69619190fd69ffc5a02fc61
EZmodeApps <75046274+EZmodeApps@users.noreply.github.com>
2022-03-23 17:22:32 +0300

    ex01 almost done

--- ex01/contact.cpp
+++ ex01/contact.cpp
@@ -8,9 +8,9 @@ Contact::~Contact() {}
 void	Contact::showContact(void)
 {
 	std::cout << "|" << std::setw(10) << id;
-	for (int i = 0; i < 3; i++) // 3 - wtf
+	for (int i = 0; i < 3; i++) // 5
 	{
-		if (info[i].size() > 10)
+		if (info[i].length() > 10)
 			std::cout << "|" << info[i].substr(0, 9) << ".";
 		else
 			std::cout << "|" << std::setw(10) << info[i];
@@ -20,38 +20,30 @@ void	Contact::showContact(void)
 }
 
 bool Contact::setContact(int index) {
-	std::string input;
-//	if (index > 3) // 8
-//		id = 0;
-//	else
 		id = index;
-//	std::getline(std::cin, input);
 	for (int i = 0; i <= 2; i++)
 	{
-		std::cin >> input;
-//		std::cout << i <<"count\n";
-		std::cout << "Enter name: ";
-//		std::getline(std::cin, info[i]);
-		info[i] = input;
+		if (i == 0)
+		{
+			std::cout << "Enter name: ";
+			std::getline(std::cin, info[i]);
+
+		}
 		if (i == 1)
 		{
 			std::cout << "Enter last name: ";
-//			std::getline(std::cin, info[i]);
-			info[i] = input;
+			std::getline(std::cin, info[i]);
 		}
 		else if (i == 2)
 		{
 			std::cout << "Enter nickname: ";
-//			std::getline(std::cin, info[i]);
-			info[i] = input;
+			std::getline(std::cin, info[i]);
 		}
-//		if (i == 0)
-//			std::getline(std::cin, info[i]);
 		if (info[i].empty() == 1)
 		{
 			return (false);
 		}
-		std::cin.clear();
+//		std::cin.clear();
 	}
 	return (true);
 }
--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -1,33 +1,34 @@
 #include "phonebook.hpp"
 
-phonebook::phonebook() : index(-1), amount(0) {}
+phonebook::phonebook() : index(0), amount(0) {}
 
 phonebook::~phonebook() {}
 
 void phonebook::add_contact(void)
 {
-		//std::cin.clear();
-		index++;
-//	bool result = contacts[++index].setContact(index);
-	if (index > 2) //8
+	if (index == 2) //8
 		index = 0;
-	contacts[index].setContact(index); //если строка не пустая то увеличиваем счетчик
-//	if (contacts[index].setContact(index) && index > 8) {
-//		index = 0;
-//	}
-//	else
-//		std::cout << "Error: contact can't be empty!" << std::endl;
+	bool result = contacts[index].setContact(index);
+	if (!result)
+	{
+		std::cout << "Error: contact can't be empty!" << std::endl;
+		return;
+	}
+	if (result && amount < 2)
+	{
+		amount++;
+		index++;
+	}
+	else
+		index++;
 }
 
 void 	phonebook::search(void)
 {
-	std::cin.clear();
 	std::cout << "|-------------------------------------------|" << std::endl;
 	std::cout << "|     Index|First Name| Last Name|  Nickname|" << std::endl;
 	std::cout << "|-------------------------------------------|" << std::endl;
-	for (int i = 0; i <= amount; i++) { //amount
+	for (int i = 0; i < amount; i++) {
 		contacts[i].showContact();
-//		std::cout << i << std::endl;
 	}
-//	std::cout << "index - "<< index << std::endl;
 }
